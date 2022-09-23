a09c658745c60a0aa836381bcaa544c97c10f082
Ivan Balabay <i.balabai@gmail.com>
2022-09-23 20:27:17 +0300

    Exception handling added for incorrect type of value entered by user.

--- Seminar01/hw1-1.py
+++ Seminar01/hw1-1.py
@@ -1,5 +1,9 @@
-day = int(input("Input a day of the week: "))
-if int(day) < 1 or int(day) > 7:
-    print("Not a day of the week!")
-else:
-    print("Workday" if 1 <= int(day) <= 5 else "Weekend")
+# noinspection PyBroadException
+try:
+    day = int(input("Input a day of the week: "))
+    if int(day) < 1 or int(day) > 7:
+        print("Not a day of the week!")
+    else:
+        print("Workday" if 1 <= int(day) <= 5 else "Weekend")
+except:
+    print("Incorrect type of value, pls input only integer value!")
