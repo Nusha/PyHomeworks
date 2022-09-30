b1b5424d1ff4247d48c65c57c73000d9c1542887
Ivan Balabay <i.balabai@gmail.com>
2022-10-01 00:06:55 +0300

    Task 3 for homework 2 completed.

---
+++ Seminar02/Task03/hw2-3.py
@@ -0,0 +1,13 @@
+# Задайте список из n чисел, заполненный по формуле (1 + 1/n) ** n и выведите на экран их сумму.
+# Для n = 6: [2, 2, 2, 2, 2, 3] -> 13
+
+
+def sum_of_list(num):
+    numlist = []
+    for i in range(1, num + 1):
+        numlist.append(round((1 + 1 / i) ** i))
+    return numlist
+
+
+n = int(input("Input N  "))
+print(f"{sum_of_list(n)} -> {sum(sum_of_list(n))}")
