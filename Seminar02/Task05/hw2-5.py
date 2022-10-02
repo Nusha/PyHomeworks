b8b8125872f73655fb3ee0473dea311402527fe6
Ivan Balabay <i.balabai@gmail.com>
2022-10-02 16:08:23 +0300

    Task 5, homework 2 completed.

--- Seminar02/Task05/hw2-5.py
+++ Seminar02/Task05/hw2-5.py
@@ -2,5 +2,28 @@
 # 10
 # -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # -> [0, 7, 6, 3, 4, 2, 9, 5, 1, 8]
+import random
+
+def make_my_list(num):
+    mylist = []
+    for i in range(num):
+        mylist.append(i)
+    return mylist
 
 
+def my_shuffle(mylist):
+    for i in range(len(mylist)):
+        j = (random.randint(0, len(mylist)-1))
+        tmp = mylist[j]
+        mylist[j] = mylist[i]
+        mylist[i] = tmp
+    return mylist
+
+try:
+    size = int(input("Enter list size: "))
+
+    nlist = make_my_list(size)
+    print(nlist)
+    print(my_shuffle(nlist))
+except ValueError:
+    print("Please input only integers.")
