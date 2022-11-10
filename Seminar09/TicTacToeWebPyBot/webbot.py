233701863c643969387dd424ef6bdc40acec596a
Ivan Balabay <i.balabai@gmail.com>
2022-11-10 19:27:08 +0300

    Fixes to webbot.py

--- Seminar09/TicTacToeWebPyBot/webbot.py
+++ Seminar09/TicTacToeWebPyBot/webbot.py
@@ -28,7 +28,6 @@ logger = logging.getLogger(__name__)
 
 # Define a `/start` command handler.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
     await update.message.reply_text(
         "Please press the button to play game.",
         reply_markup=ReplyKeyboardMarkup.from_button(
@@ -40,26 +39,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
-# Handle incoming WebAppData
-async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Print the received data and remove the button."""
-    # Here we use `json.loads`, since the WebApp sends the data JSON serialized string
-    # (see webappbot.html)
-    data = json.loads(update.effective_message.web_app_data.data)
-    await update.message.reply_html(
-        text=f"You selected the color with the HEX value <code>{data['hex']}</code>. The "
-             f"corresponding RGB value is <code>{tuple(data['rgb'].values())}</code>.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token("token").build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
+
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
